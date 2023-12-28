c09f3788c254d9cb4b3b3f9cb31afa498a96238e
ryande55 <buatgithub23@gmail.com>
2023-12-28 20:08:24 +0700

    insert view model 1

--- app/src/main/java/com/example/databaseapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/databaseapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -33,27 +33,4 @@ data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
 
-data class InsertUiEvent(
-    val id: Int = 0,
-    val nama: String = "",
-    val email: String = "",
-    val nohp: String = ""
-)
 
-fun InsertUiEvent.toKontak(): Kontak = Kontak(
-    id = id,
-    nama = nama,
-    alamat = email,
-    telpon = nohp
-)
-
-fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
-    insertUiEvent = toInsertUiEvent()
-)
-
-fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
-    id = id,
-    nama = nama,
-    email = alamat,
-    nohp = telpon
-)
